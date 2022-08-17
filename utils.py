63c02170e5792bc85965b8344162ee3b80661886
José Miguel Quintana <josemiguelquinta@gmail.com>
2022-08-16 22:59:50 -0400

    feat(top-days): implement func to get top days by tweets amount

--- utils.py
+++ utils.py
@@ -18,3 +18,18 @@ def get_top_users_by_tweets(tweets, amount):
     top_users = sorted(users_array, key=lambda user: user[1], reverse=True)[:amount]
     top_usernames = [user[0] for user in top_users]
     return top_usernames
+
+
+def get_top_days_by_tweets(tweets, amount):
+    days = {}
+    for tweet in tweets:
+        day = tweet["date"][:10]
+        if day not in days:
+            days[day] = 1
+        else:
+            days[day] += 1
+
+    days_array = list(days.items())
+    top_days = sorted(days_array, key=lambda day: day[1], reverse=True)[:amount]
+    top_days = [day[0] for day in top_days]
+    return top_days
