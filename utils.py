52b888f96acbd5d25835803515e7d118ba572c41
José Miguel Quintana <josemiguelquinta@gmail.com>
2022-08-16 22:53:01 -0400

    feat(top-users): implement get_top_users_by tweets func

--- utils.py
+++ utils.py
@@ -3,3 +3,18 @@ def get_top_retweeted(tweets, amount):
         tweets, key=lambda tweet: tweet["retweetCount"], reverse=True
     )[:amount]
     return top_retweeted
+
+
+def get_top_users_by_tweets(tweets, amount):
+    users = {}
+    for tweet in tweets:
+        username = tweet["user"]["username"]
+        if username not in users:
+            users[username] = 1
+        else:
+            users[username] += 1
+
+    users_array = list(users.items())
+    top_users = sorted(users_array, key=lambda user: user[1], reverse=True)[:amount]
+    top_usernames = [user[0] for user in top_users]
+    return top_usernames
