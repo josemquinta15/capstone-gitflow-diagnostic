aea1e302bad4449699ac945f010ea8c57e3a4831
José Miguel Quintana <josemiguelquinta@gmail.com>
2022-08-16 23:00:52 -0400

    feat(top-days): integrate top days func into main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# Los top 10 tweets más retweeted.
-# Los top 10 usuarios en función de la cantidad de tweets que emitieron.
 # Los top 10 días donde hay más tweets.
 # Los top 10 hashtags más usados.
 
 import json
-from utils import get_top_retweeted, get_top_users_by_tweets
+from utils import (
+    get_top_retweeted,
+    get_top_users_by_tweets,
+    get_top_days_by_tweets,
+)
 
 
 def get_tweets(filename):
@@ -17,6 +19,7 @@ def main():
     tweets = get_tweets("test_tweets.json")
     print(get_top_retweeted(tweets, 10))
     print(get_top_users_by_tweets(tweets, 10))
+    print(get_top_days_by_tweets(tweets, 10))
 
 
 if __name__ == "__main__":
